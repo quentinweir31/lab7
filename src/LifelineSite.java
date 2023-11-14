e30bda71fe8fa7766fe0d57e5a1c1152e038df60
Quentin <quentin3weir3@gmail.com>
2023-11-14 15:44:12 -0500

    refactored the getBillableAmount method

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -1,13 +1,18 @@
 public class LifelineSite extends Site{
-    private int _units;
-    private float _rate;
+    private static int _units;
+    private static float _rate;
     public LifelineSite(int units, float rate) {
         this._units = units;
         this._rate = rate;
     }
     public double getBillableAmount() {
-        double base = _units * _rate * 0.5;
+        double base = getBase();
         double tax = base * Site.TAX_RATE * 0.2;
         return base + tax;
+    }
+
+    private static double getBase() {
+        double base = _units * _rate * 0.5;
+        return base;
     }
 }
